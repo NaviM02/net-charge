425d10f83b3e2d041545e0fe1dd44b30d5708ed0
Hatsune02 <tzunundonavin@gmail.com>
2025-07-08 19:07:53 -0600

    Change de navbar and the pages for clients

--- frontend-net-charge/src/pages/ClientDetailPage.tsx
+++ frontend-net-charge/src/pages/ClientDetailPage.tsx
@@ -1,8 +1,37 @@
+import { useParams } from "react-router-dom";
+import { useClients } from "../hooks/useClients";
+import { ClientInfo } from "../components/ClientInfo";
+import { ListOfPayments } from "../components/ListOfPayments";
+import { usePayments } from "../hooks/usePayments";
+
 export default function ClientDetailPage() {
+  const { id } = useParams();
+  const { getClient, updateClient } = useClients();
+  const { getPaymentsByClient } = usePayments();
+
+  // VALIDAR QUE ID ES NUMERO
+  
+
+  const client = getClient(Number(id));
+  if (!client) return <p>Cliente no encontrado.</p>;
+
   return (
-    <div>
-      <h1>Detalles cliente</h1>
-      {/* Aquí va tu contenido */}
+    <div className="page pt-6 pl-16 pr-14">
+      <h1 className="text-3xl font-bold text-gray-800">
+          Información del cliente
+      </h1>
+
+      <section className="grid grid-cols-1 md:grid-cols-12 gap-6 pt-8">
+        <div className="md:col-span-3">
+          <ClientInfo client={client} onSave={updateClient} />
+        </div>
+        <div className="md:col-span-9">
+          <h1 className="text-xl font-bold text-gray-800 pb-6">PAGOS</h1>
+          <div className="h-[400px] overflow-y-auto pr-2">
+            <ListOfPayments payments={getPaymentsByClient(Number(id))} />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- frontend-net-charge/src/pages/ClientsPage.tsx
+++ frontend-net-charge/src/pages/ClientsPage.tsx
@@ -1,7 +1,39 @@
+import { Link } from "react-router-dom";
+import { ListOfClients } from "../components/ListOfClients";
+import { useClients } from "../hooks/useClients";
+import "./pages-styles/ClientsPage.css"
+
 export default function ClientsPage() {
+  const { clients } = useClients();
+
+
   return (
-    <div className="min-h-screen bg-gray-100 flex justify-center items-center">
-      <h1 className="text-3xl font-bold text-blue-600">Test Tailwind</h1>
+    <div className="page pt-6 pl-18 pr-18">
+      <h1 className="text-3xl font-bold text-gray-800">
+        Lista de clientes
+      </h1>
+      <div className="flex items-center">
+        <Link 
+          to="/clientes"
+          className=""
+        >
+          Agregar cliente
+        </Link>
+        <form className="flex items-center gap-3 mb-6 ml-auto w-fit">
+          <input placeholder="Nombre del cliente"
+            className="px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent w-64"
+          />
+          <button type="submit"
+            className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors"
+          >
+            Buscar
+          </button>
+        </form>
+      </div>
+      
+      <main>
+        <ListOfClients clients={ clients } />
+      </main>
     </div>
   );
 }
